6a05062b92a91c53bafe150ef07ff0fd15d136e2
shuaiwei.he <shuaiwei.he@jarvismedical.com>
2025-04-15 11:51:14 +0800

    add

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>My Name is Hsw</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count} ++++++
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
